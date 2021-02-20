02d7ed035a95ebb577597ab7f97037875e7b1546
Denis <deniskrasheninnikov73@gmail.com>
2021-02-20 19:47:32 +0400

    Добавил второй урок

---
+++ lesson_2.php
@@ -0,0 +1,18 @@
+<?php 
+
+$age = 27;
+$str = "Мой возраст $age лет<br>";
+echo $str;
+
+$float = 3.14;
+echo $float;
+
+$bool = false;
+echo $bool;
+
+$empty = null;
+echo $empty;
+
+// Null означает "ничего", когда False означает "неверно в логическом контексте". 
+
+?>
